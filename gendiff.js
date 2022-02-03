823f0a850e1286cd1833c0945acbbedf7c8a5133
Andrew52F <81577250+Andrew52F@users.noreply.github.com>
2022-02-03 19:57:55 +0300

    make gendiff function

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,15 @@
-
+#!/usr/bin/env node
 const program = require('commander');
+const gendiff = require('./index');
 
 program
-.version('0.0.1')
-.description('Compares two configuration files and shows a difference.')
-.argument('<filepath1>')
-.argument('<filepath2>')
-.option('-f, --format <type>', 'output file format')
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output file format')
+  .action((filepath1, filepath2) => {
+    gendiff(filepath1, filepath2);
+  });
 
-program.parse();
+program.parse(process.argv);
---
+++ index.js
@@ -0,0 +1,43 @@
+const _ = require('lodash');
+const path = require('path');
+const fs = require('fs');
+
+const getAbsPath = (fpath) => {
+  let filepath = fpath;
+  if (!path.isAbsolute(filepath)) {
+    filepath = path.resolve([process.cwd(), filepath]);
+  }
+  return filepath;
+};
+
+const getObjFromFilePath = (fpath) => {
+  let filepath = fpath;
+  filepath = getAbsPath(filepath);
+  const fileData = fs.readFileSync(filepath);
+  const obj = JSON.parse(fileData);
+  return obj;
+};
+
+const gendiff = (filepath1, filepath2) => {
+  const obj1 = getObjFromFilePath(filepath1);
+  const obj2 = getObjFromFilePath(filepath2);
+
+  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
+  const resultObj = keys.reduce((previous, curr) => {
+    const prev = previous;
+    if (obj1[curr] === obj2[curr]) {
+      prev[`  ${curr}`] = obj1[curr];
+    } else if (!obj2[curr]) {
+      prev[`- ${curr}`] = obj1[curr];
+    } else if (!obj1[curr]) {
+      prev[`+ ${curr}`] = obj2[curr];
+    } else {
+      prev[`- ${curr}`] = obj1[curr];
+      prev[`+ ${curr}`] = obj2[curr];
+    }
+    return prev;
+  }, {});
+  const resultStr = JSON.stringify(resultObj, true, 1).replace(/[,"]/g, '');
+  return console.log(resultStr);
+};
+module.exports = gendiff;
