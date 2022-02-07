dff241508b41f0741804900c196133ad9b16707f
Andrew52F <81577250+Andrew52F@users.noreply.github.com>
2022-02-07 07:04:46 +0300

    fix

--- __test__/genDiff.test.js
+++ __test__/genDiff.test.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/genDiff';
+import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
