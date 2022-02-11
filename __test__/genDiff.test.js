2bb3665ccffb7c82f905326215423d8a264cfc5c
Andrew52F <81577250+Andrew52F@users.noreply.github.com>
2022-02-11 08:19:35 +0300

    Delete old tests

--- __test__/genDiff.test.js
+++ __test__/genDiff.test.js
@@ -10,32 +10,18 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('genDiff testing', () => {
-  test('JSON fixtures', () => {
-    const file1Path = getFixturePath('file1.json');
-    const file2Path = getFixturePath('file2.json');
-    const actual = genDiff(file1Path, file2Path);
-    const correct = readFile('correct.txt');
-    expect(actual).toEqual(correct);
-  });
-  test('YAML fixtures', () => {
-    const file1Path = getFixturePath('file1.yml');
-    const file2Path = getFixturePath('file2.yaml');
-    const actual = genDiff(file1Path, file2Path);
-    const correct = readFile('correct.txt');
-    expect(actual).toEqual(correct);
-  });
   test('Recursive JSON fixtures', () => {
-    const file1path = getFixturePath('bigfile1.json');
-    const file2path = getFixturePath('bigfile2.json');
+    const file1path = getFixturePath('file1.json');
+    const file2path = getFixturePath('file2.json');
     const actual = genDiff(file1path, file2path);
-    const correct = readFile('bigcorrect.txt');
+    const correct = readFile('correct.txt');
     expect(actual).toEqual(correct);
   });
   test('Recursive YAML fixtures', () => {
-    const file1path = getFixturePath('bigfile1.yaml');
-    const file2path = getFixturePath('bigfile2.yml');
+    const file1path = getFixturePath('file1.yaml');
+    const file2path = getFixturePath('file2.yml');
     const actual = genDiff(file1path, file2path);
-    const correct = readFile('bigcorrect.txt');
+    const correct = readFile('correct.txt');
     expect(actual).toEqual(correct);
   });
 });
